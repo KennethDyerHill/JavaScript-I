5ba7ffaadff36e64452a8acaf4cfc407bd35fcd0
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2019-07-22 18:25:00 -0400

    Update 22-7-19 1824

--- assignments/objects.js
+++ assignments/objects.js
@@ -70,9 +70,22 @@ console.log(aDaine.gender)
     // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
     // console.log(kennan.speak());
 
+    const kennan = {
+      name: "Kennan",
+      speak: function() {
+        return "Hello, my name is Kennan."
+      }
+
+    };
+    console.log(kennan.speak());
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-
+const multiplyNums = function(num1, num2){
+  return num1 * num2;
+  
+}
+console.log(multiplyNums(3,4));
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
